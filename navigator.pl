8cd74a03c97c3fcdc83e86208856e2292d85e93a
ttopuz <43010843+ttopuz@users.noreply.github.com>
2019-11-23 15:11:44 -0800

    Update navigator.pl

--- navigator.pl
+++ navigator.pl
@@ -1,55 +1,60 @@
-%course (major, required course, year)
-course(cpen, cpen211, 5, core).
-course(cpen, cpen221, 4, core).
-course(cpen, cpen281, 3, core).
-course(cpen, cpsc261, 4, core).
-course(cpen, elec201, 4, core).
-course(cpen, cpen291, 6, core).
-course(cpen, math220, 3, core).
-course(cpen, math253, 3, core).
-course(cpen, math256, 3, core).
+
+%-----------------------------------------------
+%main data included
+
+
+%course (major, course code, credits, required or not, year)
+course(cpen, cpen211, 5, core, 2).
+course(cpen, cpen221, 4, core, 2).
+course(cpen, cpen281, 3, core, 2).
+course(cpen, cpsc261, 4, core, 2).
+course(cpen, elec201, 4, core, 2).
+course(cpen, cpen291, 6, core, 2).
+course(cpen, math220, 3, core, 2).
+course(cpen, math253, 3, core, 2).
+course(cpen, math256, 3, core, 2).
 
 
 %3rd year cpen
-course(cpen, cpen311, 4, core).
-course(cpen, cpen331, 4, core).
-course(cpen, cpen391, 6, core).
-course(cpen, cpsc221, 4, core).
-course(cpen, elec221, 4, core).
-course(cpen, Elective, 3, core):- Elective=math318; Elective=stat251; Elective=math302; Elective=stat302.
+course(cpen, cpen311, 4, core, 3).
+course(cpen, cpen331, 4, core, 3).
+course(cpen, cpen391, 6, core, 3).
+course(cpen, cpsc221, 4, core, 3).
+course(cpen, elec221, 4, core, 3).
+course(cpen, Elective, 3, core, 3):- Elective=math318; Elective=stat251; Elective=math302; Elective=stat302.
 
 
 
 %4th year cpen
-course(cpen, apsc450, 2, core).
-course(cpen, cpen481, 3, core).
-course(cpen, cpen491, 10, core).
+course(cpen, apsc450, 2, core, 4).
+course(cpen, cpen481, 3, core, 4).
+course(cpen, cpen491, 10, core, 4).
 
 %cpen advanced
 
 
 
 %breadth electives
-course(cpen, elec202, 4, electives).
-course(cpen, elec301, 4, electives).
-course(cpen, elec315, 4, electives).
-course(cpen, cpen321, 4, electives).
-course(cpen, elec331, 4, electives).
+course(cpen, elec202, 4, electives, [Y]):- member(Y, [3,4])
+course(cpen, elec301, 4, electives, [Y]):- member(Y, [3,4])
+course(cpen, elec315, 4, electives, [Y]):- member(Y, [3,4])
+course(cpen, cpen321, 4, electives, [Y]):- member(Y, [3,4])
+course(cpen, elec331, 4, electives, [Y]):- member(Y, [3,4])
 
 %science electives
-course(cpen, phys250, 3, science_electives). 
-course(cpen, phys200, 3, science_electives).
-course(cpen, phys330, 3, science_electives).
-course(cpen, phys333, 3, science_electives).
-course(cpen, phys404, 3, science_electives).
-course(cpen, biol111, 3, science_electives).
-course(cpen, biol112, 3, science_electives).
-course(cpen, biol121, 3, science_electives).
-course(cpen, biol123, 3, science_electives).
-course(cpen, biol345, 3, science_electives).
-course(cpen, chem201, 3, science_electives).
-course(cpen, chem251, 3, science_electives).
-course(cpen, chem260, 3, science_electives).
+course(cpen, phys250, 3, science_electives, 4). 
+course(cpen, phys200, 3, science_electives, 4).
+course(cpen, phys330, 3, science_electives, 4).
+course(cpen, phys333, 3, science_electives, 4).
+course(cpen, phys404, 3, science_electives, 4).
+course(cpen, biol111, 3, science_electives, 4).
+course(cpen, biol112, 3, science_electives, 4).
+course(cpen, biol121, 3, science_electives, 4).
+course(cpen, biol123, 3, science_electives, 4).
+course(cpen, biol345, 3, science_electives, 4).
+course(cpen, chem201, 3, science_electives, 4).
+course(cpen, chem251, 3, science_electives, 4).
+course(cpen, chem260, 3, science_electives, 4).
 
 
 
